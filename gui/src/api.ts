1c9a2ba7c9226b79d6a4f5a35965ce46fc7e94ba
Sam Partee <partees21@gmail.com>
2022-06-27 12:37:27 -0700

    Simplify API calls

--- gui/src/api.ts
+++ gui/src/api.ts
@@ -1,74 +1,88 @@
 import { MASTER_URL } from './config';
+import { logout } from './auth';
 
-// get products from Redis through the FastAPI backend
-export const getProducts = async (limit=20, skip=0) => {
-  const response = await fetch(MASTER_URL + '?limit=' + limit + '&skip=' + skip);
+export const fetchFromBackend = async (url: string, method: string, body?: any) => {
+  const request = new Request(url, {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem("token")}`
+    },
+  });
+
+  const response = await fetch(request);
+
+  if (response.status === 500) {
+    throw new Error('Internal server error');
+  }
+  if (response.status === 401 || response.status === 403) {
+    logout();
+    // redirect to home page
+    window.location.href = "/";
+  }
+
   const data = await response.json();
-  if (data) { return data; }
 
-  return Promise.reject('Failed to get message from backend');
-};
+  if (response.status > 400 && response.status < 500) {
+    if (data.detail) {
+      throw data.detail;
+    }
+    throw data;
+  }
 
-export const getProductsByText = async (search_text: string, limit=20, skip=0) => {
+  return data;
+}
+export const getProducts = async (limit=15, skip=0) => {
+  return fetchFromBackend(`${MASTER_URL}?limit=${limit}&skip=${skip}`, 'GET');
+}
+// get products from Redis through the FastAPI backend
+
+export const getProductsByText = async (search_text: string, limit=15, skip=0) => {
   // TODO use limit and skip to paginate through search results
   let body = {
     text: search_text,
     number_of_results: limit
   }
 
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-};
-
-  const response = await fetch(MASTER_URL + 'search?limit=' + limit, requestOptions);
-  const data = await response.json();
-  if (data) { return data; }
-
-  return Promise.reject('Failed to search for products from backend');
+  const url = MASTER_URL + 'search?limit=' + limit;
+  return fetchFromBackend(url, 'POST', body);
 };
 
 
-export const getVisuallySimilarProducts = async (id: number, search='KNN', limit=20, skip=0) => {
-
+export const getVisuallySimilarProducts = async (id: number,
+                                                search='KNN',
+                                                gender="",
+                                                category="",
+                                                limit=15,
+                                                skip=0) => {
   let body = {
     product_id: id,
     search_type: search,
+    gender: gender,
+    category: category,
     number_of_results: limit
   }
 
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-};
-
-  const response = await fetch(MASTER_URL + "vectorsearch/image", requestOptions);
-  const data = await response.json();
-  if (data) { return data; }
-
-  return Promise.reject('Failed to get similar products from backend');
+  const url = MASTER_URL + "vectorsearch/image";
+  return fetchFromBackend(url, 'POST', body);
 };
 
 
-export const getSemanticallySimilarProducts = async (id: number, search='KNN', limit=20, skip=0) => {
-
+export const getSemanticallySimilarProducts = async (id: number,
+                                                    search='KNN',
+                                                    gender="",
+                                                    category="",
+                                                    limit=15,
+                                                    skip=0) => {
   let body = {
     product_id: id,
     search_type: search,
+    gender: gender,
+    category: category,
     number_of_results: limit
   }
 
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-};
-
-  const response = await fetch(MASTER_URL + "vectorsearch/text", requestOptions);
-  const data = await response.json();
-  if (data) { return data; }
-
-  return Promise.reject('Failed to get similar products from backend');
+  const url = MASTER_URL + "vectorsearch/text";
+  return fetchFromBackend(url, 'POST', body);
 };
